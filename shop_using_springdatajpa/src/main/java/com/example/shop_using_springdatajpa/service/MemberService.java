e17147cd717de51500d5eb2d33ccca6f78dfbc9f
hyun6ik <fkanwm@naver.com>
2021-02-20 23:03:54 +0900

    Member Service 개발 validateDuplicateMember()는 내가 한번 만들어봄

---
+++ shop_using_springdatajpa/src/main/java/com/example/shop_using_springdatajpa/service/MemberService.java
@@ -0,0 +1,47 @@
+package com.example.shop_using_springdatajpa.service;
+
+import com.example.shop_using_springdatajpa.domain.Member;
+import com.example.shop_using_springdatajpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    //회원가입
+    @Transactional
+    public Long join(Member member){
+
+        validateDuplicateMember(member);
+        Member savedMember = memberRepository.save(member);
+        return savedMember.getId();
+    }
+
+    // 중복회원 검증
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findMemberById(member.getId());
+        if(!StringUtils.isEmpty(findMember.getUsername())){
+            throw new IllegalStateException("이미 존재하는 회원입니다!");
+        }
+    }
+
+    // 회원 전체 조회
+    public List<Member> findAllMember(){
+        return memberRepository.findAll();
+
+    }
+
+    // 회원 조회
+    public Member findMember(Long id){
+       return memberRepository.findMemberById(id);
+    }
+}

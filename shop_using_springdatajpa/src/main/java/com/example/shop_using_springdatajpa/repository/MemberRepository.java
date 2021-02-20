f706b065a5a34df18b23302f30c857c92f4e06b8
hyun6ik <fkanwm@naver.com>
2021-02-20 22:32:51 +0900

    Member Repository 개발

--- shop_using_springdatajpa/src/main/java/com/example/shop_using_springdatajpa/repository/MemberRepository.java
+++ shop_using_springdatajpa/src/main/java/com/example/shop_using_springdatajpa/repository/MemberRepository.java
@@ -2,9 +2,15 @@ package com.example.shop_using_springdatajpa.repository;
 
 import com.example.shop_using_springdatajpa.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Member findMemberById(Long id);
 
+    @Query("select m from Member m where m.username =:username")
+    List<Member> findByName(@Param("username") String username);
 }

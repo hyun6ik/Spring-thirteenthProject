fc3ea9ae9455538e2fff232f3c2b296615f4283b
hyun6ik <fkanwm@naver.com>
2021-02-21 00:07:04 +0900

    Item Service 개발

---
+++ shop_using_springdatajpa/src/main/java/com/example/shop_using_springdatajpa/service/ItemService.java
@@ -0,0 +1,31 @@
+package com.example.shop_using_springdatajpa.service;
+
+import com.example.shop_using_springdatajpa.domain.item.Item;
+import com.example.shop_using_springdatajpa.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly=true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findById(itemId).orElse(null);
+    }
+
+}

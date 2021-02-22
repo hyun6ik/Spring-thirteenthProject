1a088574eff39e8e4ac9a4e5c7da3cdfcfa246a1
hyun6ik <fkanwm@naver.com>
2021-02-22 16:32:26 +0900

    회원 등록

--- shop_using_springdatajpa/src/main/java/com/example/shop_using_springdatajpa/controller/MemberController.java
+++ shop_using_springdatajpa/src/main/java/com/example/shop_using_springdatajpa/controller/MemberController.java
@@ -1,10 +1,16 @@
 package com.example.shop_using_springdatajpa.controller;
 
+import com.example.shop_using_springdatajpa.domain.Address;
+import com.example.shop_using_springdatajpa.domain.Member;
 import com.example.shop_using_springdatajpa.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,5 +23,24 @@ public class MemberController {
     public String createForm(Model model){
         model.addAttribute("memberForm" ,new MemberForm());
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result){
+
+        if(result.hasErrors()){
+            return "/members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+
     }
 }
